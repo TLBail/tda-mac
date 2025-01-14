539b39e98fa770879ac10aff24ddeec19bba1a38
tholeb <43311443+tholeb@users.noreply.github.com>
2025-01-14 13:21:28 +0100

    add node script

---
+++ working_stage/node.py
@@ -0,0 +1,15 @@
+from lib.ahoi.modem.modem import Modem
+from src.NodeTDAMAC import NodeTDAMAC
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(f"Usage: python3 {sys.argv[0]} <serialport> <modem_id_in_byte>")
+        sys.exit(1)
+
+    modem = Modem()
+    modem.connect(sys.argv[1])
+    modem.receive(True)
+    
+    node = NodeTDAMAC(modem, int(sys.argv[2]))
